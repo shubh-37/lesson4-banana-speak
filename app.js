9db8963b0c07b4c7b7aa85ff08462f9dac817acc
shubh-37 <shubh.arya37@gmail.com>
2022-09-26 09:54:29 +0530

    reading input from user

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+var btnTranslate = document.querySelector("#btn-translate");
+var inputTxt = document.querySelector("#input-txt");
+
+function clickHandler(){
+    var translatedVal = inputTxt.value;
+    console.log(translatedVal); 
+}
+
+btnTranslate.addEventListener("click", clickHandler);
