33d751885fb30c42346673e4ae6d831ca4cb4827
shubh-37 <shubh.arya37@gmail.com>
2022-09-27 10:55:49 +0530

    feat: fetching the minion url

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputTxt = document.querySelector("#input-txt");
+var outputDiv = document .querySelector("#output-txt");
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function translatedUrl(text){
+    return url + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Oops :( error occured " + error);
+    alert("Something is wrong with the server right now!! Please try again in sometime.");
+}
 
 function clickHandler(){
-    var translatedVal = inputTxt.value;
-    console.log(translatedVal); 
+    //taking input
+    var inputText = inputTxt.value;
+    //processing the input 
+    
+
+    fetch(translatedUrl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedVal = json.contents.translated;
+        outputDiv.innerHTML = translatedVal
+    })
+    .catch(errorHandler)
+     
 }
 
 btnTranslate.addEventListener("click", clickHandler);
